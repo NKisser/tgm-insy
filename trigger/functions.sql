6a943be097b3045c3ba9ac9a63d694605737611e
asuender <asuender@student.tgm.ac.at>
2023-03-24 09:04:24 +0100

    Finish functions task

--- trigger/functions.sql
+++ trigger/functions.sql
@@ -1,14 +1,21 @@
-CREATE OR REPLACE FUNCTION bilanz_mit_steuer (client_id INTEGER)
+CREATE OR REPLACE FUNCTION bilanz_mit_steuer(client_id INTEGER)
     RETURNS INTEGER
-    AS $$
-    DECLARE
-       einzahlungen DECIMAL;
-       auszahlungen DECIMAL;
-    BEGIN
-       SELECT SUM(amount) INTO einzahlungen FROM transfers WHERE to_client_id = client_id;
-       SELECT SUM(amount) INTO auszahlungen FROM transfers WHERE from_client_id = client_id;
-       RETURN einzahlungen - auszahlungen;
-    END;
-    $$ LANGUAGE plpgsql;
+AS
+$$
+DECLARE
+    einzahlungen DECIMAL;
+    auszahlungen DECIMAL;
+    einzahlungen2 DECIMAL;
+    auszahlungen2 DECIMAL;
+BEGIN
+    SELECT SUM(amount) * 0.98 INTO einzahlungen FROM transfers WHERE to_client_id = client_id AND date < '2020-01-01';
+    SELECT SUM(amount) * 0.98 INTO auszahlungen FROM transfers WHERE from_client_id = client_id AND date < '2020-01-01';
+
+    SELECT SUM(amount) * 0.99 INTO einzahlungen2 FROM transfers WHERE to_client_id = client_id AND date >= '2020-01-01';
+    SELECT SUM(amount) * 0.99 INTO auszahlungen2 FROM transfers WHERE from_client_id = client_id AND date >= '2020-01-01';
+
+    RETURN (einzahlungen + einzahlungen2) - (auszahlungen + auszahlungen2);
+END;
+$$ LANGUAGE plpgsql;
 
 SELECT bilanz_mit_steuer(1);
